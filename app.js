627b8e24fdf72d953c9e3cad46beaa85f2eecce7
ZarroukHassene <hassene.zarrouk@esprit.tn>
2023-07-03 15:54:46 +0100

    tst3

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const scene = document.querySelector('a-scene');
-  
-    const marker = document.createElement('a-marker');
-    marker.setAttribute('type', 'hiro');
-  
-    const video = document.createElement('a-video');
-  video.setAttribute('position', '0 0 0');
+  const scene = document.querySelector('a-scene');
+
+  const marker = document.createElement('a-marker');
+  marker.setAttribute('type', 'hiro');
+
+  const video = document.createElement('a-video');
+  video.setAttribute('position', '0 0 -1.2');
   video.setAttribute('rotation', '-90 0 0');
   video.setAttribute('width', '2');
-  video.setAttribute('height', '1.2');
+  video.setAttribute('height', '2');
   video.setAttribute('src', './media/1_sekou.mp4');
   video.setAttribute('visible', 'false');
 
   marker.appendChild(video);
 
   marker.addEventListener('markerFound', () => {
-    video.setAttribute('visible', 'true');
-    video.play();
+    if (!video.isPlaying) {
+      video.setAttribute('visible', 'true');
+      video.play();
+    }
   });
 
   marker.addEventListener('markerLost', () => {
@@ -25,5 +27,4 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   scene.appendChild(marker);
-  });
-  
+});
