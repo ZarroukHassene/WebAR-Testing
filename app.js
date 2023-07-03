c6a629392082aa827918acb85f1303bd5587a1af
ZarroukHassene <hassene.zarrouk@esprit.tn>
2023-07-03 15:28:47 +0100

    video test

--- app.js
+++ app.js
@@ -4,27 +4,26 @@ document.addEventListener('DOMContentLoaded', function () {
     const marker = document.createElement('a-marker');
     marker.setAttribute('type', 'hiro');
   
-    const plane = document.createElement('a-plane');
-    plane.setAttribute('position', '0 0 0');
-    plane.setAttribute('rotation', '-90 0 0');
-    plane.setAttribute('width', '1.6');
-    plane.setAttribute('height', '1.6');
-    plane.setAttribute('color', 'white');
-    plane.setAttribute('opacity', '0');
-    plane.setAttribute('visible', 'false');
-  
-    marker.appendChild(plane);
-  
-    marker.addEventListener('markerFound', () => {
-      plane.setAttribute('visible', 'true');
-      plane.setAttribute('opacity', '0.8');
-    });
-  
-    marker.addEventListener('markerLost', () => {
-      plane.setAttribute('visible', 'false');
-      plane.setAttribute('opacity', '0');
-    });
-  
-    scene.appendChild(marker);
+    const video = document.createElement('a-video');
+  video.setAttribute('position', '0 0 0');
+  video.setAttribute('rotation', '-90 0 0');
+  video.setAttribute('width', '2');
+  video.setAttribute('height', '1.2');
+  video.setAttribute('src', './media/1_sekou.mp4');
+  video.setAttribute('visible', 'false');
+
+  marker.appendChild(video);
+
+  marker.addEventListener('markerFound', () => {
+    video.setAttribute('visible', 'true');
+    video.play();
+  });
+
+  marker.addEventListener('markerLost', () => {
+    video.setAttribute('visible', 'false');
+    video.pause();
+  });
+
+  scene.appendChild(marker);
   });
   
