7a6b4972257347128851e7472f86308140aeaf65
ZarroukHassene <hassene.zarrouk@esprit.tn>
2023-07-21 02:14:21 +0100

    fixed bugs, playlist working

--- app.js
+++ app.js
@@ -1,113 +1,126 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const playPauseBtn = document.getElementById("playPauseBtn");
-  const playPauseIcon = document.getElementById("playPauseIcon");
-  const muteUnmuteBtn = document.getElementById("muteUnmuteBtn");
-  const muteUnmuteIcon = document.getElementById("muteUnmuteIcon");
-  const nextBtn = document.getElementById("nextBtnn");
-  const prevBtn = document.getElementById("prevBtn");
-   video = document.querySelector("a-video");
+// Sample video URLs
+const videoUrls = [
+  "./media/1_sekou.mp4",
+  "./media/2_naim.mp4",
+  "./media/3_juliette.mp4"
+];
+
+//Set up the playlist div by populating the li element by the video URLs
+const playlist = document.getElementById("playlist");
+for (let i = 0; i < videoUrls.length; i++) {
+  
+  const li = document.createElement("li");
+  li.textContent = videoUrls[i];
+  playlist.appendChild(li);
+}
+
+//Function to update the playlist div to show the currently playing video
+function updatePlaylist() {
+  const li = playlist.getElementsByTagName("li");
+  for (let i = 0; i < li.length; i++) {
+    if (i == currentIndex) {
+      li[i].style.fontStyle = "italic";
+      li[i].style.fontSize = 1.3 + "em";
+    } else {
+      li[i].style.fontStyle = "normal";
+      li[i].style.fontSize = 1 + "em";
+    }
+  }
+}
+
+// Set up the video player
 
 
- 
-  const marker = document.querySelector("a-marker");
+let currentIndex = 0;
+const videoPlayer = document.getElementById("videoPlayer");
+const playPauseBtn = document.getElementById("playPauseBtn");
+const playPauseIcon = document.getElementById("playPauseIcon");
+const muteUnmuteBtn = document.getElementById("muteUnmuteBtn");
+const muteUnmuteIcon = document.getElementById("muteUnmuteIcon");
+
+let lastPlaybackPosition = 0;
+const marker = document.querySelector("a-marker");
   let isPlaying = false;
   let markerVisible = false;
   let isMuted = false;
 
+function playVideo() {
+  videoPlayer.src = videoUrls[currentIndex];
+  videoPlayer.currentTime = lastPlaybackPosition;
+  videoPlayer.play();
+  playPauseIcon.src = "./media/pauseBtn.png";
+  isPlaying = true;
+}
 
-let vids = ["1_sekou.mp4", "2_naim.mp4", "3_juliette.mp4"];
-  let i = 0;
-  
-  // video.setAttribute('src', './media/' + vids[i]);
+function pauseVideo() {
+  videoPlayer.pause();
+  lastPlaybackPosition = videoPlayer.currentTime;
+  playPauseIcon.src = "./media/playBtn.png";
+  isPlaying = false;
+}
 
-  //Initially video is paused
-  document
-  .querySelector(document.querySelector("a-video").getAttribute("src"))
-  .pause();
-
-  //Periodically (every 100ms) checks if the video is playing and modifies the text of id=playPauseText accordingly
-  setInterval(() => {
-    if (isPlaying) {
-      playPauseText.innerText = "Playing";
-    } else {
-      playPauseText.innerText = "Paused";
-    }
-  }, 100);
-
-//
-
-//Toggles the video between playing and paused
-  function toggleVideo() {
-    if (isPlaying) {
-      document
-        .querySelector(document.querySelector("a-video").getAttribute("src"))
-        .pause();
-      playPauseIcon.src = "./media/playBtn.png";
-      isPlaying = false;
-    } else if(!isPlaying) {
-      document
-        .querySelector(document.querySelector("a-video").getAttribute("src"))
-        .play();
-      playPauseIcon.src = "./media/pauseBtn.png";
-      isPlaying = true;
-    }
+function togglePlayPause() {
+  if (videoPlayer.paused || videoPlayer.ended) {
+    playVideo();
+    isPlaying = true;
+  } else {
+    pauseVideo();
+    isPlaying = false;
   }
+}
 
-  //Toggles the video display based on the marker visibility
-  function handleVideoDisplay() {
-    if (markerVisible) {
-      video.setAttribute("visible", "true");
-      toggleVideo();
-    } else {
-      video.setAttribute("visible", "false");
-      toggleVideo();
-    }
+function toggleMuteUnmute() {
+  if (isMuted) {
+    videoPlayer.muted = false;
+    muteUnmuteIcon.src = "./media/unmutedBtn.png";
+  } else {
+    videoPlayer.muted = true;
+    muteUnmuteIcon.src = "./media/mutedBtn.png";
   }
-  //Toggles the video sound on and off via button of id=muteUnmuteIcon
+  isMuted = !isMuted;
+}
 
-  function toggleSound() {
-    if(!isMuted) {
-      document.querySelector(document.querySelector("a-video").getAttribute("src")).muted = true;
-      muteUnmuteIcon.src = "./media/mutedBtn.png";
-      isMuted = true;
-    } else {
-      document.querySelector(document.querySelector("a-video").getAttribute("src")).muted = false;
-      muteUnmuteIcon.src = "./media/unmutedBtn.png";
-      isMuted = false;
-    }
+function nextVideo() {
+  currentIndex = (currentIndex + 1) % videoUrls.length;
+  playVideo();
+  isPlaying = true;
+  updatePlaylist();
+}
+
+function previousVideo() {
+  currentIndex = (currentIndex - 1 + videoUrls.length) % videoUrls.length;
+  playVideo();
+  isPlaying = true;
+  updatePlaylist();
+}
+
+function autoPause() {
+  if (markerVisible) {
+    pauseVideo();
+  } else {
+    playVideo();
   }
-  
-  function nextVideo() {
+}
 
-    
-    //Increment the video index
-    console.log(video.getAttribute('src'));
-    if (i < vids.length - 1) {
-      i++;
-      video.setAttribute('src', './media/' + vids[i]);
-    } else {
-      i = 0;
-      video.setAttribute('src', './media/' + vids[i]);
-    }
 
- 
-  }
 
-  // AR marker visibility event listeners
+// AR marker visibility event listeners
 
-  marker.addEventListener("markerFound", () => {
-    markerVisible = true;
-    handleVideoDisplay();
-  });
-  marker.addEventListener("markerLost", () => {
-    markerVisible = false;
-    handleVideoDisplay();
-  });
-
-  playPauseBtn.addEventListener("click", toggleVideo);
-  muteUnmuteBtn.addEventListener("click", toggleSound);
- nextBtn.addEventListener('click', nextVideo);
-  
-   
- 
+marker.addEventListener("markerFound", () => {
+  markerVisible = true;
+  playVideo();
 });
+marker.addEventListener("markerLost", () => {
+  markerVisible = false;
+  pauseVideo();
+
+});
+
+// Add event listeners for buttons
+document.getElementById("nextBtn").addEventListener("click", nextVideo);
+document.getElementById("previousBtn").addEventListener("click", previousVideo);
+playPauseBtn.addEventListener("click", togglePlayPause);
+muteUnmuteBtn.addEventListener("click", toggleMuteUnmute);
+
+// Start playing the first video in the playlist
+pauseVideo();
